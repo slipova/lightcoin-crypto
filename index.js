cb06419ac3b0075fad8434d5d521c632ebf7947e
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-06 01:51:05 +0000

    create a function

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+    // this.transactions = [];
+  }
+
+  // get balance() {
+  // }
+
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
+  }
+
+}
+
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol")
 
-t1 = new Withdrawal(50.25);
+const t1 = new Deposit(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(79.99, myAccount); //returns weird balance
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+const t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
